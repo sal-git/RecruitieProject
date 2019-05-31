f9b43c3c7d36bc3b38136e6da33af1cbbd730b64
sal <stephenlightcap121212@gmail.com>
2019-05-31 00:29:33 -0500

    basic structure for notes closes #14

--- frontend/src/components/CandidateView.vue
+++ frontend/src/components/CandidateView.vue
@@ -12,6 +12,8 @@
                 </div>
             </div>
         </section>
+        <br>
+        <br>
         <section>
             <div class="control">
                 <div class="tags has-addons">
@@ -32,13 +34,47 @@
                 </div>
             </div>
         </section>
+        <br>
+        <br>
+        <section>
+            <NoteForm noteType="candidate" />
+        </section>
+        <br>
+        <br>
+        <section>
+            <CandidateNotes :rows="rowNotes" />
+        </section>
     </div>
 </template>
 
 <script>
+    import CandidateNotes from './NoteList.vue';
+    import NoteForm from './notes/NoteForm.vue';
+    import axios from 'axios'
+
     export default {
         name: "CandidateView",
-        props: ['candidate']
+        props: ['candidate'],
+        data() {
+            return {
+                rowNotes: []
+            }
+        },
+        components: {
+            CandidateNotes,
+            NoteForm
+        },
+        mounted() {
+            this.getNotes()
+        },
+        methods: {
+            getNotes() {
+                axios
+                    .get('/api/candidates-notes', { crossdomain: true })
+                    .then(response => {this.rowNotes = response.data})
+            }
+        }
+        
     }
 </script>
 
---
+++ frontend/src/components/NoteList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <vue-good-table
+            :columns="columns"
+            :rows="rows"/>
+    </div>
+</template>
+
+<script>
+    import 'vue-good-table/dist/vue-good-table.css';
+    import { VueGoodTable } from 'vue-good-table';
+    import axios from 'axios'
+
+    
+    export default {
+        name: 'NoteList',
+        props: ['rows'],
+        data(){
+            return {
+                columns: [
+                    {
+                        label: 'Note',
+                        field: 'note',
+                    },
+                    {
+                        label: 'Date',
+                        field: 'date',
+                        type: 'date',
+                        dateInputFormat: 'YYYY-MM-DD',
+                        dateOutputFormat: 'MMM Do YY',
+                    },
+                ],
+                // rows: this.rows,
+            };
+        },
+        components: {
+            VueGoodTable,
+        },
+        mounted() {
+            
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
